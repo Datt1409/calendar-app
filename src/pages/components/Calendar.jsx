f86eeec97452de12ab235257a51248ac1730e184
Datt1409 <112248757+Datt1409@users.noreply.github.com>
2023-09-25 16:55:38 +0700

    update calendar

--- src/pages/components/Calendar.jsx
+++ src/pages/components/Calendar.jsx
@@ -1,7 +1,6 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 import {
-  daysOfWeek,
   YEARS,
   renderCalendar,
   compareMonth,
@@ -10,32 +9,30 @@ import {
 } from "../utils";
 
 export default function Calendar({
-  isMultiple = true,
+  multiple = true,
   locale = "en-US",
   onChooseDate,
+  activeDate = new Date(),
+  showCalendar = false,
+  setShowCalendar,
 }) {
   const today = new Date();
   const [month, setMonth] = useState(today.getMonth() + 1);
   const [year, setYear] = useState(today.getFullYear());
   const [selectedDays, setSelectedDays] = useState([]);
 
+  console.log(showCalendar);
+
   const calendarDays = useMemo(
     () => renderCalendar(month, year),
     [year, month]
   );
+  const localizedMonths = useMemo(() => getLocalizeMonth(year, locale), [year]);
 
-  const localizedMonths = useMemo(
-    () => getLocalizeMonth(year, locale),
-    [year, locale]
-  );
-
-  const localizedDays = useMemo(
-    () => getLocalizedDay(year, month, locale),
-    [locale]
-  );
+  const localizedDays = getLocalizedDay(year, month, locale);
 
   const handleClick = (clickedDay) => {
-    if (isMultiple) {
+    if (multiple) {
       const isSelected = selectedDays.some(
         (selectedDay) => selectedDay === clickedDay
       );
@@ -100,85 +97,99 @@ export default function Calendar({
     setYear(selectedYear);
   };
 
+  const handleHide = () => {
+    if (showCalendar) setShowCalendar(false);
+  };
+
   return (
-    <div className="w-[384px] h-[400px] p-2 flex flex-col justify-center text-center align-center border rounded-lg ml-10">
-      {/* <div className="flex flex-row justify-center text-center items-center p-5 text-3xl font-bold"></div> */}
+    <>
+      <div className="w-[384px] h-[400px] p-2 flex flex-col justify-center text-center align-center border rounded-lg ml-10 z-10">
+        <div className="flex flex-row justify-center items-center p-5">
+          <AiOutlineLeft
+            size={18}
+            className="cursor-pointer"
+            onClick={onPrevMonth}
+          />
+          <select
+            className="rounded-md border bg-slate-100 p-2 mr-2 ml-4 cursor-pointer"
+            value={localizedMonths[month - 1]}
+            onChange={onChangeMonth}
+          >
+            {localizedMonths.map((localizedMonth, index) => (
+              <option key={index} value={localizedMonth}>
+                {localizedMonth}
+              </option>
+            ))}
+          </select>
+          <select
+            className="rounded-md border bg-slate-100 p-2 mr-4 ml-2 cursor-pointer"
+            value={year}
+            onChange={onChangeYear}
+          >
+            {YEARS.map((year, index) => (
+              <option key={index} value={year}>
+                {year}
+              </option>
+            ))}
+          </select>
+          <AiOutlineRight
+            size={18}
+            className="cursor-pointer"
+            onClick={onNextMonth}
+          />
+        </div>
 
-      <div className="flex flex-row justify-center items-center p-5">
-        <AiOutlineLeft
-          size={18}
-          className="cursor-pointer"
-          onClick={onPrevMonth}
-        />
-        <select
-          className="rounded-md border bg-slate-100 p-2 mr-2 ml-4 cursor-pointer"
-          value={localizedMonths[month - 1]}
-          onChange={onChangeMonth}
-        >
-          {localizedMonths.map((localizedMonth, index) => (
-            <option key={index} value={localizedMonth}>
-              {localizedMonth}
-            </option>
+        <div className="grid grid-cols-7 text-center items-center mb-2">
+          {localizedDays.map((localizedDay, index) => (
+            <div key={index} className="font-bold">
+              {localizedDay}
+            </div>
           ))}
-        </select>
-        <select
-          className="rounded-md border bg-slate-100 p-2 mr-4 ml-2 cursor-pointer"
-          value={year}
-          onChange={onChangeYear}
-        >
-          {YEARS.map((year, index) => (
-            <option key={index} value={year}>
-              {year}
-            </option>
-          ))}
-        </select>
-        <AiOutlineRight
-          size={18}
-          className="cursor-pointer"
-          onClick={onNextMonth}
-        />
-      </div>
+        </div>
 
-      <div className="grid grid-cols-7 text-center items-center mb-2">
-        {localizedDays.map((localizedDay, index) => (
-          <div key={index} className="font-bold">
-            {localizedDay}
-          </div>
-        ))}
-      </div>
-
-      <div className="grid grid-cols-7 text-center">
-        {calendarDays.map((calendarDay, index) => (
-          <div
-            key={index}
-            className={`flex justify-center items-center h-10 w-10 text-center ml-2 mb-2 rounded-full cursor-pointer
+        <div className="grid grid-cols-7 text-center">
+          {calendarDays.map((calendarDay, index) => (
+            <div
+              key={index}
+              className={`flex justify-center items-center h-10 w-10 text-center ml-2 mb-2 rounded-full cursor-pointer
             ${
-              isMultiple &&
+              multiple &&
               selectedDays.some((day) => day === calendarDay.value) &&
               compareMonth(calendarDay.value, month)
                 ? "bg-blue-500 text-white"
                 : ""
             } 
             ${
-              !isMultiple &&
+              !multiple &&
               selectedDays.some((day) => day === calendarDay.value) &&
               compareMonth(calendarDay.value, month)
                 ? "bg-blue-500 text-white"
                 : ""
             }
             ${
-              compareMonth(calendarDay.value, month)
-                ? "text-neutral-600 hover:bg-gray-300 hover:text-white"
+              compareMonth(calendarDay.value, month) &&
+              calendarDay.value.toDateString() !== activeDate.toDateString()
+                ? "text-neutral-600 hover:bg-blue-500 hover:text-white"
                 : "text-neutral-400"
-            }
-           
+            } 
+            ${
+              calendarDay.value.toDateString() === activeDate.toDateString() &&
+              activeDate.toDateString()
+                ? "bg-red-400 text-white hover:none"
+                : ""
+            }  
             `}
-            onClick={() => handleClick(calendarDay.value)}
-          >
-            {calendarDay.label}
-          </div>
-        ))}
+              onClick={() => handleClick(calendarDay.value)}
+            >
+              {calendarDay.label}
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+      <div
+        className="fixed top-0 left-0 w-full h-full z-0"
+        onClick={handleHide}
+      ></div>
+    </>
   );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,49 +4,41 @@ import { AiOutlineCalendar } from "react-icons/ai";
 
 export default function Home() {
   const [showCalendar, setShowCalendar] = useState(false);
-  const [fromDate, setFromDate] = useState(new Date().toLocaleDateString());
+  const [fromDate, setFromDate] = useState(new Date());
 
-  // const formatDate = (selectedDate) => {
-  //   if (!selectedDate) return "";
-
-  //   const year = new Date().getFullYear().toString();
-  //   const month = (new Date().getMonth() + 1).toString().padStart(2, 0);
-  //   const date = new Date().getDate().toString().padStart(2, 0);
-
-  //   return `${date} - ${month} - ${year}`;
-  // };
-
+  const handleSetShowCalendar = () => {
+    setShowCalendar(!showCalendar);
+  };
   const handleSelectDate = (selectedDate) => {
-    setFromDate(selectedDate.toLocaleDateString());
+    setFromDate(selectedDate);
   };
 
   return (
-    <main className="mt-20 flex flex-row">
-      <Calendar isMultiple />
-      <div className="flex flex-col ml-10">
+    <main className="mt-20 flex flex-row h-screen">
+      <div className="flex flex-col ml-10 w-full">
         <div className="flex flex-row mb-3">
           <input
-            className="ml-10 border-2 w-80 h-10 rounded-md p-4 placeholder:uppercase"
+            className="ml-10 border-2 w-80 h-10 rounded-md p-4"
             type="text"
-            // value={
-            //   fromDate.getDate().toString().padStart(2, 0) +
-            //   "-" +
-            //   (fromDate.getMonth() + 1).toString().padStart(2, 0) +
-            //   "-" +
-            //   fromDate.getFullYear().toString()
-            // }
-            value={fromDate}
+            value={fromDate.toLocaleDateString()}
             readOnly
           />
           <button
             className="bg-black w-15 h-10 text-white border rounded-md p-2"
-            onClick={() => setShowCalendar(!showCalendar)}
+            onClick={() => handleSetShowCalendar()}
           >
             <AiOutlineCalendar size={20} />
           </button>
         </div>
+
         {showCalendar && (
-          <Calendar isMultiple={false} onChooseDate={handleSelectDate} />
+          <Calendar
+            multiple={false}
+            activeDate={fromDate}
+            onChooseDate={handleSelectDate}
+            showCalendar={showCalendar}
+            setShowCalendar={setShowCalendar}
+          />
         )}
       </div>
     </main>
